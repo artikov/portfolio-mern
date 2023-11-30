ba130f193ac10c73ab626d9b4d7efe2cb49bf8ad
artikov <oybekartikov@icloud.com>
2023-11-30 16:59:35 +0500

    Navbar mobile update

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,52 +1,71 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
+
 	const navDefault =
-		"text-sm  leading-6 px-4 py-2 border-r border-slate-800 hover:bg-slate-800 hover:text-white hover:ease-in-out duration-300";
+		"text-sm leading-6 px-4 py-2 md:border-r border-slate-800 hover:bg-slate-800 hover:text-white hover:ease-in-out duration-300";
 	const navActive = navDefault + " border-b border-b-orange-300 text-white";
 
 	return (
-		<header className="border-b border-slate-800 top-0 absolute w-full">
-			<nav className="mx-auto flex items-center justify-between text-slate-500">
-				<div className="flex md:flex-1">
+		<header className="border-b border-slate-800 top-0 absolute w-full bg-slate-900 md:bg-transparent">
+			<nav className="text-slate-500 flex flex-col md:flex-row ">
+				<NavLink
+					to={`/`}
+					className={
+						navDefault + " w-full md:w-auto pr-32 p-2 border-r-0 md:border-r"
+					}
+				>
+					Oybek Artikov
+				</NavLink>
+
+				{/* MOBILE NAVIGATION BUTTON */}
+				<div className="md:hidden border-l border-slate-800">
+					<button className="px-4 py-2" onClick={toggleMenu}>
+						Menu
+					</button>
+				</div>
+
+				<div
+					className={`md:flex ${
+						menuOpen ? "flex flex-col" : "md:flex-row hidden"
+					}`}
+				>
 					<NavLink
 						to={`/`}
-						className={navDefault + " pr-28 p-2 border-r-0 md:border-r"}
+						className={({ isActive }) => (isActive ? navActive : navDefault)}
 					>
-						Oybek Artikov
+						_home
 					</NavLink>
-					<div className="hidden md:flex">
-						<NavLink
-							to={`/`}
-							className={({ isActive }) => (isActive ? navActive : navDefault)}
-						>
-							_home
-						</NavLink>
-						<NavLink
-							to={`about`}
-							className={({ isActive }) => (isActive ? navActive : navDefault)}
-						>
-							_about
-						</NavLink>
-						<NavLink
-							to={`projects`}
-							className={({ isActive }) => (isActive ? navActive : navDefault)}
-						>
-							_projects
-						</NavLink>
-					</div>
-					<div className="hidden md:flex md:flex-1 md:justify-end">
-						<NavLink
-							to={`contact`}
-							className={({ isActive }) =>
-								isActive
-									? navActive + " border-l-2"
-									: navDefault + " border-l-2"
-							}
-						>
-							_contact
-						</NavLink>
-					</div>
+					<NavLink
+						to={`about`}
+						className={({ isActive }) => (isActive ? navActive : navDefault)}
+					>
+						_about
+					</NavLink>
+					<NavLink
+						to={`projects`}
+						className={({ isActive }) => (isActive ? navActive : navDefault)}
+					>
+						_projects
+					</NavLink>
+				</div>
+				<div className={`md:ml-auto md:flex ${menuOpen ? "block" : "hidden"}`}>
+					<NavLink
+						to={`contact`}
+						className={({ isActive }) =>
+							isActive
+								? navActive + " md:border-l-2"
+								: navDefault + " md:border-l-2"
+						}
+					>
+						_contact
+					</NavLink>
 				</div>
 			</nav>
 		</header>
@@ -54,4 +73,3 @@ const Header = () => {
 };
 
 export default Header;
-``;
