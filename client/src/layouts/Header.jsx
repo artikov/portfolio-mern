2922ae2db8b9dcbdea9211277f2a1f5ee0fb0975
artikov <oybekartikov@icloud.com>
2023-12-01 15:56:41 +0500

    Navigation responsive updated

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,10 +1,34 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 import Hamburger from "../components/Hamburger";
 
 const Header = () => {
+	const navRef = useRef(0);
+	const [navWidth, setNavWidth] = useState(0);
+
 	const [menuOpen, setMenuOpen] = useState(false);
+
+	// Handle navigation menu status on window resize
+	useEffect(() => {
+		const updateNavWidht = () => {
+			if (navRef.current) {
+				const width = navRef.current.offsetWidth;
+				setNavWidth(width);
+			}
+		};
+
+		updateNavWidht();
+
+		if (navWidth > 700) {
+			setMenuOpen(false);
+		}
+		window.addEventListener("resize", updateNavWidht);
+
+		return () => {
+			window.removeEventListener("resize", updateNavWidht);
+		};
+	}, [navRef, navWidth]);
 
 	const toggleMenu = () => {
 		setMenuOpen(!menuOpen);
@@ -14,9 +38,30 @@ const Header = () => {
 		"text-sm leading-6 px-4 py-2 md:border-r border-slate-800 hover:bg-slate-800 hover:text-white hover:ease-in-out duration-300";
 	const navActive = navDefault + " border-b border-b-orange-300 text-white";
 
+	const navMenuStyles = {
+		navOpen: {
+			maxHeight: "1000px",
+			transition: "max-height 0.6s ease-in-out",
+			overflow: "hidden",
+		},
+		navClosed: {
+			maxHeight: "40px",
+			transition: "max-height 0.6s ease-in-out",
+			overflow: "hidden",
+		},
+	};
+
 	return (
-		<header className="border-b border-slate-800 top-0 absolute w-full bg-slate-900 md:bg-transparent z-10">
-			<nav className="text-slate-500 flex flex-col md:flex-row justify-between">
+		<header
+			ref={navRef}
+			className={`border-b border-slate-800 top-0 absolute w-full bg-slate-900 md:bg-transparent z-10 h-full`}
+			style={menuOpen ? navMenuStyles.navOpen : navMenuStyles.navClosed}
+		>
+			<nav
+				className={`text-slate-500 flex flex-col md:flex-row justify-between ${
+					!menuOpen && "border-b border-slate-800"
+				}`}
+			>
 				<div className="flex">
 					<NavLink
 						to={`/`}
@@ -24,6 +69,7 @@ const Header = () => {
 							navDefault +
 							" w-full md:w-auto md:pr-28 p-2 border-r-0 md:border-r"
 						}
+						onClick={() => setMenuOpen(false)}
 					>
 						Oybek Artikov
 					</NavLink>
@@ -35,41 +81,45 @@ const Header = () => {
 				</div>
 
 				<div
-					className={`md:flex ${
+					className={`md:flex border-t border-slate-800 md:border-none ${
 						menuOpen ? "flex flex-col" : "md:flex-row hidden"
 					}`}
 				>
 					<NavLink
 						to={`/`}
 						className={({ isActive }) => (isActive ? navActive : navDefault)}
+						onClick={() => setMenuOpen(false)}
 					>
 						_home
 					</NavLink>
 					<NavLink
 						to={`about`}
 						className={({ isActive }) => (isActive ? navActive : navDefault)}
+						onClick={() => setMenuOpen(false)}
 					>
 						_about
 					</NavLink>
 					<NavLink
 						to={`projects`}
 						className={({ isActive }) => (isActive ? navActive : navDefault)}
+						onClick={() => setMenuOpen(false)}
 					>
 						_projects
 					</NavLink>
 				</div>
 				<div
 					className={`md:ml-auto md:flex md:pb-0 pb-4 ${
-						menuOpen ? "block" : "hidden"
+						menuOpen ? "flex" : "hidden"
 					}`}
 				>
 					<NavLink
 						to={`contact`}
 						className={({ isActive }) =>
 							isActive
-								? navActive + " md:border-l-2"
-								: navDefault + " md:border-l-2"
+								? navActive + " md:border-l-2 w-full"
+								: navDefault + " md:border-l-2 w-full"
 						}
+						onClick={() => setMenuOpen(false)}
 					>
 						_contact
 					</NavLink>
