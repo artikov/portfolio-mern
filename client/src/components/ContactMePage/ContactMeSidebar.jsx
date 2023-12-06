3327191890720bac975ad4c1d07a279cd9f3a3ec
artikov <oybekartikov@icloud.com>
2023-12-06 17:53:39 +0500

    Contact me page update

--- client/src/components/ContactMePage/ContactMeSidebar.jsx
+++ client/src/components/ContactMePage/ContactMeSidebar.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import Spinner from "../Spinner";
 import Message from "../Message";
 
@@ -6,13 +8,36 @@ import phone from "../../assets/icons/phone.svg";
 import linkIcon from "../../assets/icons/link.svg";
 import dropdown from "../../assets/icons/dropdown.svg";
 
+import { useMediaQuery } from "react-responsive";
+
 import { useGetContactsQuery } from "../../services/contactsApiSlice";
 
 const ContactMeSidebar = () => {
+	const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+	const [isContactsOpen, setIsContactsOpen] = useState(!isMobile);
+	const [isLinksOpen, setIsLinksOpen] = useState(!isMobile);
+
 	const { data: contacts, isLoading, error } = useGetContactsQuery();
 
 	let contactDetails = {};
 	let links = [];
+
+	const handleToggleAccordion = (type) => {
+		if (isMobile) {
+			if (type === "contacts") {
+				setIsContactsOpen(!isContactsOpen);
+			} else if (type === "links") {
+				setIsLinksOpen(!isLinksOpen);
+			}
+		}
+	};
+
+	useEffect(() => {
+		if (!isMobile) {
+			setIsContactsOpen(true);
+		}
+	}, [isMobile]);
+
 	if (!isLoading) {
 		contactDetails = contacts[0];
 		links = contacts[0].otherLinks;
@@ -25,11 +50,18 @@ const ContactMeSidebar = () => {
 	return (
 		<div>
 			<div>
-				<div className="text-white text-sm flex gap-2 p-2.5 border-b border-slate-800">
+				<div
+					className={`text-white text-sm flex gap-2 p-2.5 border-b border-slate-800`}
+					onClick={() => handleToggleAccordion("contacts")}
+				>
 					<img src={dropdown} />
 					contacts
 				</div>
-				<div className="m-4 text-slate-500 text-sm flex flex-col gap-4">
+				<div
+					className={`m-4 text-slate-500 text-sm flex flex-col gap-4 accordion-content ${
+						isContactsOpen ? "open" : ""
+					}`}
+				>
 					{isLoading ? (
 						<Spinner />
 					) : (
@@ -54,11 +86,18 @@ const ContactMeSidebar = () => {
 				</div>
 			</div>
 			<div>
-				<div className="text-white text-sm flex gap-2 p-2.5 border-b border-t border-slate-800">
+				<div
+					className="text-white text-sm flex gap-2 p-2.5 border-b border-t border-slate-800"
+					onClick={() => handleToggleAccordion("links")}
+				>
 					<img src={dropdown} />
 					find-me-also
 				</div>
-				<div className="m-4 text-slate-500 flex flex-col gap-2 text-sm">
+				<div
+					className={`m-4 text-slate-500 flex flex-col gap-2 text-sm accordion-content ${
+						isLinksOpen ? "open" : ""
+					}`}
+				>
 					{links?.map((link) => (
 						<a
 							href={link}
--- client/src/pages/ContactMePage.jsx
+++ client/src/pages/ContactMePage.jsx
@@ -4,13 +4,14 @@ import ContactMeSidebar from "../components/ContactMePage/ContactMeSidebar";
 
 const ContactMePage = () => {
 	return (
-		<div className="flex h-full">
+		<div className="flex flex-col md:flex-row h-full">
+			<div className="md:hidden text-white p-4">_contacts</div>
 			<div className="border-r border-slate-800 min-w-[238px]">
 				<ContactMeSidebar />
 			</div>
 			<div className="w-full">
 				<div className="text-white text-sm border-b border-slate-800">
-					<div className="border-r border-slate-800 p-2.5 flex justify-between max-w-[159px]">
+					<div className="border-r border-slate-800 p-2.5 hidden md:flex justify-between max-w-[159px]">
 						contacts <span className="text-slate-600">x</span>
 					</div>
 				</div>
@@ -18,7 +19,7 @@ const ContactMePage = () => {
 					<div className="md:border-b lg:border-r lg:w-1/2 border-slate-800">
 						<ContactMeForm />
 					</div>
-					<div className="hidden sm:block lg:w-1/2 xl:mt-12">
+					<div className="hidden md:block lg:w-1/2 xl:mt-12">
 						<ContactMeCodeSnippet />
 					</div>
 				</div>
