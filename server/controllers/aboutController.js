5744577799208aba8b11cf516edf3eaf01dabc76
Oybek Artikov <oybekartikov@icloud.com>
2024-01-01 20:53:41 +0500

    Admin > Abouts backend update

--- server/controllers/aboutController.js
+++ server/controllers/aboutController.js
@@ -13,4 +13,40 @@ const getAbouts = asyncHandler(async (req, res) => {
 	}
 });
 
-export { getAbouts };
+// @desc 		Add category to about
+// @route		POST /api/abouts/add-category/:id
+// @access	Private/Admin
+const addCategoryToAbout = asyncHandler(async (req, res) => {
+	const { category, content } = req.body;
+	const about = await About.findById(req.params.id);
+
+	const existingCategory = about.categories.find(
+		(cat) => cat.category === category
+	);
+
+	if (existingCategory) {
+		existingCategory.content = content;
+	} else {
+		about.categories.push({ category, content });
+	}
+
+	const updatedAbout = await about.save();
+
+	res.status(201).json(updatedAbout);
+});
+
+// @desc 		Add image to about
+// @route		POST /api/abouts/add-image/:id
+// @access	Private/Admin
+const addImageToAbout = asyncHandler(async (req, res) => {
+	const { image, caption } = req.body;
+	const about = await About.findById(req.params.id);
+
+	about.images.push({ image, caption });
+
+	const updatedAbout = await about.save();
+
+	res.status(201).json(updatedAbout);
+});
+
+export { getAbouts, addCategoryToAbout, addImageToAbout };
--- server/routes/aboutRoutes.js
+++ server/routes/aboutRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getAbouts } from "../controllers/aboutController.js";
+import {
+	getAbouts,
+	addCategoryToAbout,
+	addImageToAbout,
+} from "../controllers/aboutController.js";
 
 const router = express.Router();
 
 router.route("/").get(getAbouts);
+router.route("/add-category/:id").patch(addCategoryToAbout);
+router.route("/add-image/:id").patch(addImageToAbout);
 
 export default router;
