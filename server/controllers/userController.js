45e96e058d7d46bcb61f71e73942ec5643a3829d
artikov <artikov1208@gmail.com>
2024-02-02 21:12:12 +0500

    Login & Logout Backend implemented

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -41,7 +41,30 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route		GET /api/users/login
 // @access	Private
 const authenticateUser = asyncHandler(async (req, res) => {
-	// Your code for user authentication goes here
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+
+	if (user && (await user.matchPassword(password))) {
+		generateToken(res, user._id);
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		res.status(401);
+		throw new Error("Invalid email or password");
+	}
 });
 
-export { registerUser, authenticateUser };
+// @desc   Logout user / clear cookie
+// @route  POST /api/users/logout
+// @access Private
+const logoutUser = asyncHandler(async (req, res) => {
+	res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) });
+	res.status(200).json({ message: "Logged out" });
+});
+
+export { registerUser, authenticateUser, logoutUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import {
+	registerUser,
+	authenticateUser,
+	logoutUser,
+} from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(registerUser);
+router.route("/login").post(authenticateUser);
+router.route("/logout").post(logoutUser);
 
 export default router;
